024b6f21af26ccbbc521fd53cef1964cc02caffe
Jeremy Fix <jeremy.fix@gmail.com>
2019-03-30 15:55:37 +0100

    upd

--- socketserver/echo_server.py
+++ socketserver/echo_server.py
@@ -20,12 +20,24 @@ def decode_image_buffer(img_buffer):
     # Decode a colored image
     return cv2.imdecode(img_array, 1)
 
-def encode_image(cv2_img):
-    global encode_params
+def encode_image(cv2_img, jpeg_quality):
+    encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
     result, buf = cv2.imencode('.jpg', cv2_img, encode_params)
     return buf.tobytes()
 
+
+def image_process(cv2_img):
+    # For fun, we play with the image
+    cv2_img = 255 - cv2_img
+
+    return cv2_img
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    def __init__(self, request, client_address, server, jpeg_quality, image_process):
+        self.jpeg_quality = jpeg_quality
+        self.image_process = image_process
+        super(MyTCPHandler, self).__init__(request, client_address, server)
 
     def handle(self):
         print("Connection established")
@@ -41,12 +53,12 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                 if cmd != 'done!':
                     raise RuntimeError('Unexpected client info. Expected "done!", go "{}"'.format(cmd))
 
-
-                # For fun, we play with the image
-                # and make its negative
+                # Decode the image
                 cv2_img = decode_image_buffer(img_bytes)
-                cv2_img = 255 - cv2_img
-                img_bytes = encode_image(cv2_img)
+                # Make some processing on it
+                cv2_img = self.image_process(cv2_img)
+                # JPEG encode the result
+                img_bytes = encode_image(cv2_img, self.jpeg_quality)
 
                 # Build up the reply encoding the image
                 msg = bytes('image{:010}'.format(len(img_bytes)), "ascii")
@@ -69,9 +81,10 @@ args = parser.parse_args()
 host = get_ip(args.interface)
 port = args.port
 
-encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), args.jpeg_quality]
 
-with socketserver.TCPServer((host, port), MyTCPHandler) as server:
+MyHandlerBuilder = lambda request, client_address, server: MyTCPHandler(request, client_address, server, args.jpeg_quality, image_process)
+
+with socketserver.TCPServer((host, port), MyHandlerBuilder) as server:
     print("Server listening on {}:{}".format(host, port))
     server_running = True
     while server_running:
